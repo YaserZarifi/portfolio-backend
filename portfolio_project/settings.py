43c51eaa74c650ed6ba0b0e575cacb074d69cc54
YaserZarifi <yaserzarifi1378@gmail.com>
2025-09-15 19:31:06 +0330

    settings updated

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -145,7 +145,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://yaserzarifi.netlify.app/"
+    "https://yaserzarifi.netlify.app"
 ]
 
 
